b58684cb602ee84005dc8455ed4ae716d8a736f8
VINAY KUMAR SINGH <119345842+Guyvinay@users.noreply.github.com>
2023-01-22 14:33:09 +0530

    cw_pr0ject_done

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-let lsData = JSON.parse(localStorage.getItem("cart")) || [];
+let data = JSON.parse(localStorage.getItem("data")) || []
+let LsData = JSON.parse(localStorage.getItem("cart")) || [];
 let api = "./obj_data/object1.json"
 async function fetchData(){
       try{
          let res = await fetch(api)
-         res = await res.json()
-         console.log(res)
+         res = await res.json()  ;      
+         localStorage.setItem("data",JSON.stringify(res))
+         //console.log(data)
          display(res)
+
       }catch(err){
            console.log(err)
       }
@@ -16,14 +19,14 @@ let div = document.getElementById("obj_1");
 
 function display(data){
 
-    data.forEach((ele,ind)=>{
+    data.forEach((ele)=>{
 
         let card = document.createElement("div");
         let img = document.createElement("img");
         let ctg = document.createElement("h3");      
         let dscr = document.createElement("p");
-        let price = document.createElement("p");
-        let rating = document.createElement("p");
+        let price = document.createElement("h4");
+        let rating = document.createElement("h4");
         let btn = document.createElement("button");
         let bkmk = document.createElement("button")
 
@@ -34,17 +37,41 @@ function display(data){
         rating.innerText =  ele.rating;
         btn.innerText = "Add to Cart"
         bkmk.innerText = "Bookmark"
-   
 
         btn.addEventListener("click",()=>{
-           lsData.push(ele);
-           localStorage.setItem("cart", JSON.stringify())
-        })
+            if(checkCart(ele)===true){
+                alert("You already Added this Product go for Another")
+            }else{
 
+                
+               LsData.push(ele)
+               //console.log(ele)
+               localStorage.setItem("cart", JSON.stringify(LsData))
+               alert("You Successfully Added in Cart")
+            }
+            
+            })
 
+       
+            bkmk.addEventListener("click",()=>{
+                let LsData =JSON.parse(localStorage.getItem("cart")) || [];
+                   LsData.push(ele)
+                   //console.log(ele)
+                   localStorage.setItem("cart", JSON.stringify(LsData))
+                });
         card.append(img,ctg,dscr,price,rating,btn,bkmk)
         div.append(card)
     })
+}
+function checkCart(e){
+    console.log(LsData)
+    for(let i=0; i<LsData.length; i++){
+        if(LsData[i].id === e.id){
+            return true
+        }
+        return false
+        //console.log(LsData[i])
+    }
 }
 
 
